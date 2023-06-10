f41b4e337817860f716ec1947cd1825725c9b96e
Caleb Frederickson <caleb.frederickson@gmail.com>
2023-06-10 08:26:02 -0800

    add Find-DockerImage

--- docker.powershell.cli.psm1
+++ docker.powershell.cli.psm1
@@ -1,4 +1,4 @@
-﻿using namespace System.Collections;
+using namespace System.Collections;
 using namespace System.Diagnostics;
 using namespace System.Collections.Generic;
 using namespace System.Management.Automation;
@@ -1314,6 +1314,68 @@ function New-DockerImage {
         )
 
         Invoke-Docker $ArgumentList -Context $Context
+    }
+}
+
+function Find-DockerImage {
+    [CmdletBinding(
+        RemotingCapability = [RemotingCapability]::OwnedByCommand,
+        PositionalBinding = $false
+    )]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $Keyword,
+
+        [Parameter()]
+        [Alias('Automated')]
+        [Nullable[bool]]
+        $IsAutomated,
+
+        [Parameter()]
+        [Alias('Official')]
+        [Nullable[bool]]
+        $IsOfficial,
+
+        [Parameter()]
+        [Alias('Stars')]
+        [int]
+        $MinimumStars,
+
+        [Parameter()]
+        [ValidateRange(1, [int]::MaxValue)]
+        [Alias('First', 'Take')]
+        [int]
+        $Limit = 100,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter([DockerContextCompleter])]
+        [string]
+        $Context
+    )
+    process {
+        $ArgumentList = @(
+            'search'
+            '--no-trunc'
+            "--limit=$Limit"
+            '--format'
+            '{{ json . }}'
+            if ($IsAutomated.HasValue) { "--filter=is-automated=$IsAutomated" }
+            if ($IsOfficial.HasValue) { "--filter=is-official=$IsOfficial" }
+            if ($MinimumStars) { "--filter=stars=$MinimumStars" }
+            $Keyword
+        )
+        $Count = 0
+        Invoke-Docker $ArgumentList -Context $Context | ForEach-Object {
+            $pso = $_ | ConvertFrom-Json
+            $pso.PSTypeNames.Insert(0, 'Docker.RemoteImage')
+            $pso
+
+            if ((++$Count) -eq $Limit -and !($PSBoundParameters.ContainsKey('Limit'))) {
+                Write-Warning "The number of results has reached the default limit of $Limit. There may be more results available. Use the -Limit parameter to increase the limit."
+            }
+        }
     }
 }
 #endregion Docker Image
