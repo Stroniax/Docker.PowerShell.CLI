5ff7ad93bf3fc40caf381a2ea5f32a3df320218b
Stroniax <caleb.frederickson@gmail.com>
2023-06-13 12:24:01 -0800

    Import-DockerImage implementation

--- docker.powershell.cli.psm1
+++ docker.powershell.cli.psm1
@@ -2009,6 +2009,70 @@ function Export-DockerImage {
     }
 
 }
+
+function Import-DockerImage {
+    [CmdletBinding(
+        DefaultParameterSetName = 'LiteralPath',
+        SupportsShouldProcess,
+        RemotingCapability = [RemotingCapability]::OwnedByCommand,
+        ConfirmImpact = [ConfirmImpact]::Low,
+        PositionalBinding = $false
+    )]
+    [OutputType('Docker.Image')]
+    [Alias('ipdi')]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = 'Path')]
+        [SupportsWildcards()]
+        [Alias('FilePath')]
+        [string]
+        $Path,
+
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'LiteralPath')]
+        [Alias('PSPath')]
+        [string]
+        $LiteralPath,
+
+        [Parameter()]
+        [switch]
+        $PassThru
+    )
+    process {
+        if ($Path) {
+            $FullPath = Resolve-Path -Path $Path
+        }
+        else {
+            $FullPath = Resolve-Path -LiteralPath $LiteralPath
+        }
+
+        if (!$?) {
+            return
+        }
+
+        # ShouldProcess
+        if (!$PSCmdlet.ShouldProcess(
+                "Importing image from '$FullPath'.",
+                "Import image from '$FullPath'?",
+                "docker image load --input '$FullPath'"
+            )) {
+            return
+        }
+
+        $ArgumentList = @(
+            'image'
+            'load'
+            '--quiet'
+            '--input'
+            $FullPath
+        )
+
+        $Output = Invoke-Docker $ArgumentList
+
+        if ($? -and $PassThru) {
+            $Id = $Output.Replace('Loaded image ID: ', '')
+            Get-DockerImage -Id $Id
+        }
+    }
+}
 #endregion Docker Image
 
 #region Docker Version
