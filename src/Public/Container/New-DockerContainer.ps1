a62ca8bdf99fbc77069cf5bc48cec7a8d4130233
Caleb Frederickson <CFrederickson@KPBSD.k12.ak.us>
2023-06-14 10:38:37 -0800

    New-DockerContainer moved to new file

---
+++ src/Public/Container/New-DockerContainer.ps1
@@ -0,0 +1,158 @@
+using namespace System.Collections.Generic
+using namespace System.Management.Automation
+using module ../../Classes/DockerContextCompleter.psm1
+using module ../../Classes/DockerImageCompleter.psm1
+
+function New-DockerContainer {
+    [CmdletBinding(
+        SupportsShouldProcess,
+        PositionalBinding = $false,
+        ConfirmImpact = [ConfirmImpact]::Medium,
+        RemotingCapability = [RemotingCapability]::OwnedByCommand)]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [ArgumentCompleter([DockerImageCompleter])]
+        [string]
+        $ImageName,
+
+        [Parameter(Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ContainerName')]
+        [string]
+        $Name,
+
+        [Parameter()]
+        [string]
+        $HostName,
+
+        [Parameter()]
+        [IPAddress]
+        $IPAddress,
+
+        [Parameter()]
+        [switch]
+        $Interactive,
+
+        [Parameter()]
+        [Hashtable]
+        $Labels,
+
+        [Parameter()]
+        [Hashtable]
+        $Environment,
+
+        [Parameter()]
+        [ValidateSet('always', 'missing', 'never')]
+        [string]
+        $PullBehavior,
+
+        [Parameter()]
+        [switch]
+        $ReadOnly,
+
+        [Parameter()]
+        [switch]
+        $AutoRemove,
+
+        [Parameter()]
+        [int]
+        $TimeoutSeconds,
+
+        [Parameter()]
+        [string]
+        $WorkingDirectory,
+
+        # Any additional parameters to pass to the docker cli
+        [Parameter()]
+        [string[]]
+        $Parameters,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter([DockerContextCompleter])]
+        [string]
+        $Context
+    )
+    process {
+        [List[string]]$ArgumentList = @(
+            'container'
+            'create'
+        )
+
+        if ($Name) {
+            $ArgumentList.Add('--name')
+            $ArgumentList.Add($Name)
+        }
+
+        if ($HostName) {
+            $ArgumentList.Add('--hostname')
+            $ArgumentList.Add($HostName)
+        }
+
+        if ($IPAddress) {
+            $ArgumentList.Add('--ip')
+            $ArgumentList.Add($IPAddress)
+        }
+
+        if ($Interactive) {
+            $ArgumentList.Add('--interactive')
+        }
+
+        if ($Labels) {
+            foreach ($k in $Labels.Keys) {
+                $ArgumentList.Add('--label')
+                $ArgumentList.Add("$k=$($Labels[$k])")
+            }
+        }
+
+        if ($Environment) {
+            foreach ($k in $Environment.Keys) {
+                $ArgumentList.Add('--env')
+                $ArgumentList.Add("$k=$($Environment[$k])")
+            }
+        }
+
+        if ($PullBehavior) {
+            $ArgumentList.Add('--pull')
+            $ArgumentList.Add($PullBehavior)
+        }
+
+        if ($ReadOnly) {
+            $ArgumentList.Add('--read-only')
+        }
+
+        if ($AutoRemove) {
+            $ArgumentList.Add('--rm')
+        }
+
+        if ($TimeoutSeconds) {
+            $ArgumentList.Add('--timeout')
+            $ArgumentList.Add($TimeoutSeconds)
+        }
+
+        if ($WorkingDirectory) {
+            $ArgumentList.Add('--workdir')
+            $ArgumentList.Add($WorkingDirectory)
+        }
+
+        if ($Parameters) {
+            foreach ($p in $Parameters) {
+                $ArgumentList.Add($p)
+            }
+        }
+
+        $ArgumentList.Add($ImageName)
+
+        if (!$PSCmdlet.ShouldProcess(
+                "Creating container '$Name' from image '$ImageName'.",
+                "Create container '$Name' from image '$ImageName'?",
+                "docker $ArgumentList")) {
+            return
+        }
+
+        $Id = Invoke-Docker $ArgumentList -Context $Context 
+        if ($?) {
+            Get-DockerContainerInternal -Id $Id -Context $Context
+        }
+    }
+}
