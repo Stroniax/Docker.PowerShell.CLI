276f4762b3879f5cb7630fb6dfb837f0c2c1d91f
Caleb Frederickson <CFrederickson@KPBSD.k12.ak.us>
2023-06-14 10:48:14 -0800

    Suspend-DockerContainer moved to new file

---
+++ src/Public/Container/Suspend-DockerContainer.ps1
@@ -0,0 +1,68 @@
+using namespace System.Management.Automation
+using module ../../Classes/DockerContainerCompleter.psm1
+using module ../../Classes/DockerContextCompleter.psm1
+
+function Suspend-DockerContainer {
+    [CmdletBinding(
+        DefaultParameterSetName = 'Id',
+        SupportsShouldProcess,
+        PositionalBinding = $false,
+        ConfirmImpact = [ConfirmImpact]::Medium,
+        RemotingCapability = [RemotingCapability]::OwnedByCommand
+    )]
+    [Alias('ssdc')]
+    param(
+        [Parameter(Position = 0, Mandatory, ParameterSetName = 'Name')]
+        [SupportsWildcards()]
+        [Alias('ContainerName')]
+        [ArgumentCompleter([DockerContainerCompleter])]
+        [string[]]
+        $Name,
+
+        [Parameter(ValueFromPipelineByPropertyName, Mandatory, ParameterSetName = 'Id')]
+        [ArgumentCompleter([DockerContainerCompleter])]
+        [Alias('Container', 'ContainerId')]
+        [string[]]
+        $Id,
+
+        [Parameter()]
+        [switch]
+        $PassThru,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter([DockerContextCompleter])]
+        [string]
+        $Context
+    )
+    process {
+        $Containers = Get-DockerContainerInternal -Id $Id -Name $Name -Context $Context -EscapeId
+
+        if ($Containers.Count -eq 0) {
+            # If no containers, the user input wildcard(s) or an error was reported by internal Get
+            Write-Verbose 'No containers to process.'
+            return
+        }
+
+        if ($Containers.Count -gt 1) {
+            $ContainerIdentification = "$($Containers.Count) containers"
+        }
+        else {
+            $ContainerIdentification = "container $($Containers.Id) ($($Containers.Names))"
+        }
+
+        if (!$PSCmdlet.ShouldProcess(
+                "Pausing all processes in $ContainerIdentification.",
+                "Pause all processes in $ContainerIdentification?",
+                "docker $ArgumentList"
+            )) {
+            return
+        }
+
+        Invoke-Docker pause $Containers.Id -Context $Context | ForEach-Object {
+            if ($PassThru) {
+                Get-DockerContainerInternal -Id $_ -Context $Context
+            }
+        }
+    }
+}
