2792a6e7d65a4c4e887aba25a53d6af221a56cdc
Caleb Frederickson <CFrederickson@KPBSD.k12.ak.us>
2023-06-14 08:55:51 -0800

    add DebugModule.psm1 to import monolith and micro modules

---
+++ debugmodule.psm1
@@ -0,0 +1,36 @@
+# Import the monolith module file and types/formats
+Update-TypeData -PrependPath $PSScriptRoot/docker.types.ps1xml
+Update-FormatData -PrependPath $PSScriptRoot/docker.formats.ps1xml
+$Root = Import-Module $PSScriptRoot/Docker.PowerShell.CLI.psm1 -PassThru
+
+# Import the micro modules
+Get-ChildItem $PSScriptRoot/src/Public -Recurse -Include '*.psm1', '*.ps1' | ForEach-Object {
+    if ($_.Extension -eq '.ps1') {
+        . $_.FullName
+    }
+    else {
+        Import-Module $_.FullName
+    }
+}
+Get-ChildItem $PSScriptRoot/src/Private -Recurse -Include '*.psm1', '*.ps1' | ForEach-Object {
+    if ($_.Extension -eq '.ps1') {
+        . $_.FullName
+    }
+    else {
+        Import-Module $_.FullName
+    }
+}
+
+# Import the micro types/formats
+Get-ChildItem $PSScriptRoot/src -Recurse -Include '*.types.ps1xml' | ForEach-Object {
+    Update-TypeData -PrependPath $_.FullName
+}
+Get-ChildItem $PSScriptRoot/src -Recurse -Include '*.formats.ps1xml' | ForEach-Object {
+    Update-FormatData -PrependPath $_.FullName
+}
+
+$ExportFunctions = @(
+    $Root.ExportedFunctions.Keys
+    Get-ChildItem $PSScriptRoot/src/Public -File -Recurse -Include '*.psm1', '*.ps1' | ForEach-Object -MemberName BaseName
+) | Where-Object { $_ }
+Export-ModuleMember -Function $ExportFunctions
