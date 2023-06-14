bc3426af543027b59d0f9eadc5816355c0d004fc
Caleb Frederickson <CFrederickson@KPBSD.k12.ak.us>
2023-06-14 10:50:50 -0800

    Wait-DockerContainer moved to new file

--- docker.powershell.cli.psm1
+++ docker.powershell.cli.psm1
@@ -19,38 +19,6 @@ using module src/Classes/DockerBuildAddHostTransformation.psm1
 
 #region Docker Container
 
-function Wait-DockerContainer {
-    [CmdletBinding(
-        DefaultParameterSetName = 'Id',
-        PositionalBinding = $false,
-        RemotingCapability = [RemotingCapability]::OwnedByCommand
-    )]
-    [Alias('wdc')]
-    param(
-        [Parameter(Mandatory, Position = 0, ParameterSetName = 'Name')]
-        [SupportsWildcards()]
-        [Alias('ContainerName')]
-        [ArgumentCompleter([DockerContainerCompleter])]
-        [string]
-        $Name,
-
-        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Id')]
-        [ArgumentCompleter([DockerContainerCompleter])]
-        [Alias('Container', 'ContainerId')]
-        [string]
-        $Id,
-
-        [Parameter()]
-        [ValidateNotNullOrEmpty()]
-        [ArgumentCompleter([DockerContextCompleter])]
-        [string]
-        $Context
-    )
-    process {
-        Invoke-Docker wait $Name -Context $Context
-    }
-}
-
 function Rename-DockerContainer {
     [CmdletBinding(
         DefaultParameterSetName = 'Id',
---
+++ src/Public/Container/Wait-DockerContainer.ps1
@@ -0,0 +1,35 @@
+using namespace System.Management.Automation
+using module ../../src/Classes/DockerContainerCompleter.psm1
+using module ../../src/Classes/DockerContextCompleter.psm1
+
+function Wait-DockerContainer {
+    [CmdletBinding(
+        DefaultParameterSetName = 'Id',
+        PositionalBinding = $false,
+        RemotingCapability = [RemotingCapability]::OwnedByCommand
+    )]
+    [Alias('wdc')]
+    param(
+        [Parameter(Mandatory, Position = 0, ParameterSetName = 'Name')]
+        [SupportsWildcards()]
+        [Alias('ContainerName')]
+        [ArgumentCompleter([DockerContainerCompleter])]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Id')]
+        [ArgumentCompleter([DockerContainerCompleter])]
+        [Alias('Container', 'ContainerId')]
+        [string]
+        $Id,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter([DockerContextCompleter])]
+        [string]
+        $Context
+    )
+    process {
+        Invoke-Docker wait $Name -Context $Context
+    }
+}
